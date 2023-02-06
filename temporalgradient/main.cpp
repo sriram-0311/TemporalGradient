6740ac57b5a1b484dd8386a721baefa48cbcaa97
sriram-0311 <114690421+sriram-0311@users.noreply.github.com>
2023-02-06 16:09:42 -0500

    creating the project

---
+++ temporalgradient/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  temporalgradient
+//
+//  Created by Anush sriram Ramesh on 2/6/23.
+//
+
+#include <iostream>
+#include "opencv2/opencv.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
