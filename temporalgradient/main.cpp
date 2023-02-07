c043c53d5e343aec455a45f07b56d207a0821a6a
sriram-0311 <114690421+sriram-0311@users.noreply.github.com>
2023-02-06 20:18:02 -0500

    part 1 completed

--- temporalgradient/main.cpp
+++ temporalgradient/main.cpp
@@ -16,7 +16,7 @@ using namespace cv;
 
 int main()
 {
-    Vector<Mat> imgs;
+    vector<Mat> imgs;
     // read all image files in the directory "dataset/Office/*.jpg in increasing order and store in a vector"
     for (int i = 1; i<=1070;i++) {
         int NumOfZeroes = 4 - to_string(i).length();
@@ -36,6 +36,24 @@ int main()
         temporal_gradient = max(temporal_gradient, diff);
     }
     imshow("temporal gradient", temporal_gradient);
+    waitKey(0);
+
+    // threshold the result to create a mask of the moving objects
+    Mat mask;
+    threshold(temporal_gradient, mask, 30, 255, THRESH_BINARY);
+
+    // combine mask with original image to highlight moving objects
+    Mat image = imread("dataset/Office/img01_0001.jpg");
+    Mat highlighted = Mat::zeros(image.size(), CV_8UC3);
+    for (int i = 0; i < image.rows; i++) {
+        for (int j = 0; j < image.cols; j++) {
+            if (mask.at<uchar>(i, j) == 255) {
+                highlighted.at<Vec3b>(i, j) = image.at<Vec3b>(i, j);
+            }
+        }
+    }
+    imshow("highlighted", highlighted);
+    waitKey(0);
     // show 10 sample images read from the directory
     // for (int i = 0; i < 10; i++) {
     //     imshow("sample images", imgs[i]);
